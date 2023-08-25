aed655f62502b02e54b0c091d7739beeb567200c
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-24 21:38:09 -0300

    update attributes

--- systemos/app/src/main/java/model/Order.java
+++ systemos/app/src/main/java/model/Order.java
@@ -11,22 +11,22 @@ import java.util.Date;
  * @author sula
  */
 public class Order {
-    
+
     private Long id;
     private String type;
     private String description;
     private String status;
+    private Date createdDate;
+    private Date statusDate;
 
-    public Order(Long id, String type, String description, String status, Date date_request, Date date_status) {
+    public Order(Long id, String type, String description, String status, Date createdDate, Date statusDate) {
         this.id = id;
         this.type = type;
         this.description = description;
         this.status = status;
-        this.date_request = date_request;
-        this.date_status = date_status;
+        this.createdDate = createdDate;
+        this.statusDate = statusDate;
     }
-    private Date date_request;
-    private Date date_status;
 
     public Long getId() {
         return id;
@@ -60,25 +60,25 @@ public class Order {
         this.status = status;
     }
 
-    public Date getDate_request() {
-        return date_request;
+    public Date getCreatedDate() {
+        return createdDate;
     }
 
-    public void setDate_request(Date date_request) {
-        this.date_request = date_request;
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
     }
 
-    public Date getDate_status() {
-        return date_status;
+    public Date getStatusDate() {
+        return statusDate;
     }
 
-    public void setDate_status(Date date_status) {
-        this.date_status = date_status;
+    public void setStatusDate(Date statusDate) {
+        this.statusDate = statusDate;
     }
 
     @Override
     public String toString() {
-        return "Order{" + "id=" + id + ", type=" + type + ", description=" + description + ", status=" + status + ", date_request=" + date_request + ", date_status=" + date_status + '}';
+        return "Order{" + "id=" + id + ", type=" + type + ", description=" + description + ", status=" + status + ", createdDate=" + createdDate + ", statusDate=" + statusDate + '}';
     }
-    
+
 }
