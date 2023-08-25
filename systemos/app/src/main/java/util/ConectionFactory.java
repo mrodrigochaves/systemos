d3bb4bb8d8400bf8dfb4bf0200d828190b89e90b
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-24 22:24:25 -0300

    add driver db properties

--- systemos/app/src/main/java/util/ConectionFactory.java
+++ systemos/app/src/main/java/util/ConectionFactory.java
@@ -10,4 +10,8 @@ package util;
  */
 public class ConectionFactory {
     
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/systemosdb";
+    public static final String USER = "admin";
+    public static final String PASS = "123456";
 }
