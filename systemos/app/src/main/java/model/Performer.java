062572f9211f67a136265a04e14bb061ccf66731
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-24 22:46:29 -0300

    update performer class

--- systemos/app/src/main/java/model/Performer.java
+++ systemos/app/src/main/java/model/Performer.java
@@ -6,10 +6,78 @@ package model;
 
 /**
  *
- * @author sula
+ * @author Márcio Rodrigo
  */
 public class Performer {
     
     private Long id;
     private String name; 
+    private String type;
+    private String description;
+    private String status;
+    private Date updatedAt;
+
+    public Performer(Long id, String name, String type, String description, String status, Date updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.status = status;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Performer{" + "id=" + id + ", name=" + name + ", type=" + type + ", description=" + description + ", status=" + status + ", updatedAt=" + updatedAt + '}';
+    }
+    
+    
 }
