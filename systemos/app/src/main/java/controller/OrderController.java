15a204fed6c16b364e79124563902700491cb94a
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-26 12:04:45 -0300

    update project

--- systemos/app/src/main/java/controller/OrderController.java
+++ systemos/app/src/main/java/controller/OrderController.java
@@ -6,20 +6,34 @@ package controller;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import model.Order;
-
+import util.ConnectionFactory;
 /**
  *
  * @author Márcio Rodrigo
  */
 public class OrderController {
     
-    public void save(Order order){}
+    public void save(Order order){
+    String sql = "INSERT INTO order(idOrder, name, description)"
+            + " VALUES (?,?,?)";
+    
+    Connection connection = null;
+    PreparedStatement statement = null;
+    
+    try { 
+        connection = ConnectionFactory.getConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setInt(1, order.getIdOrder());
+        statement.setString(2, order.getType());
+        }
+    }
     
     public void update (Order order){}
     
-    public void removeById (Long orderId){
+    public void removeById (int orderId) throws SQLException{
         String sql = "DELETE FROM order WHERE id =?";
         Connection conn = null;
         PreparedStatement statement = null;
@@ -27,8 +41,13 @@ public class OrderController {
         try {
             conn = ConnectionFactory.getConnection();
             statement = conn.prepareStatement(sql);
+            statement.setInt(1, orderId);
+            statement.execute();           
             
-        }catch(Exception e) {
+        }catch(SQLException e) {
+            throw new SQLException("Erro ao deletar a ordem");
+        } finally {
+        ConnectionFactory.closeConnection(conn);
         }
     }
     
