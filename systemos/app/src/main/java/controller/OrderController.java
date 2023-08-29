be4956e9c68932017ce24afb9fc11cd166de1ae6
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-29 19:35:42 -0300

    refactoring code

--- systemos/app/src/main/java/controller/OrderController.java
+++ systemos/app/src/main/java/controller/OrderController.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,111 +9,93 @@ import java.util.List;
 import model.Order;
 import util.ConnectionFactory;
 
-/**
- *
- * @author Márcio Rodrigo
- */
 public class OrderController {
 
     public void save(Order order) {
-        String sql = "INSERT INTO orders(orderId, type, description, status, createdAt, updateAt)"
+        String sql = "INSERT INTO orders(orderId, type, description, status, createdAt, updatedAt)"
                 + " VALUES (?,?,?,?,?,?)";
 
-        Connection connection = null;
-        PreparedStatement statement = null;
+        try (Connection connection = ConnectionFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
 
-        try {
-            connection = ConnectionFactory.getConnection();
-            statement = connection.prepareStatement(sql);
             statement.setInt(1, order.getOrderId());
             statement.setString(2, order.getType());
             statement.setString(3, order.getDescription());
             statement.setString(4, order.getStatus());
-            statement.setDate(5, new Date(order.getCreatedAt().getTime()));
-            statement.setDate(6, new Date(order.getUpdatedAt().getTime()));
-            statement.execute();
-        } catch (Exception ex) {
-            throw new RuntimeException("Erro ao deletar a ordem" + ex.getMessage(), ex);
-        } finally {
-            ConnectionFactory.closeConnection(connection, statement);
+            statement.setDate(5, new java.sql.Date(order.getCreatedAt().getTime()));
+            statement.setDate(6, new java.sql.Date(order.getUpdatedAt().getTime()));
+            statement.executeUpdate();
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error while saving order: " + ex.getMessage(), ex);
         }
     }
 
     public void update(Order order) {
         String sql = "UPDATE orders SET"
-                + "orderId = ?,"
-                + "type = ?,"
-                + "description = ?,"
-                + "status = ?,"
-                + "createdAt = ?,"
-                + "updatedAt = ?,"
-                + "WHERE id = ?";
+                + " orderId = ?,"
+                + " type = ?,"
+                + " description = ?,"
+                + " status = ?,"
+                + " createdAt = ?,"
+                + " updatedAt = ?"
+                + " WHERE id = ?";
 
-        Connection connection = null;
-        PreparedStatement statement = null;
-
-        try {
-            connection = ConnectionFactory.getConnection();
-            statement = connection.prepareStatement(sql);
+        try (Connection connection = ConnectionFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
 
             statement.setInt(1, order.getOrderId());
             statement.setString(2, order.getType());
             statement.setString(3, order.getDescription());
             statement.setString(4, order.getStatus());
-            statement.setDate(5, new Date(order.getCreatedAt().getTime()));
-            statement.setDate(6, new Date(order.getUpdatedAt().getTime()));
-            statement.execute();
+            statement.setDate(5, new java.sql.Date(order.getCreatedAt().getTime()));
+            statement.setDate(6, new java.sql.Date(order.getUpdatedAt().getTime()));
+            statement.setInt(7, order.getId());
+            statement.executeUpdate();
 
-        } catch (Exception ex) {
-            throw new RuntimeException("Erro ao atualizar a ordem" + ex.getMessage(), ex);
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error while updating order: " + ex.getMessage(), ex);
         }
     }
 
-    public void removeById(int orderId) throws SQLException {
-        String sql = "DELETE FROM orders WHERE id =?";
-        Connection connection = null;
-        PreparedStatement statement = null;
+    public void removeById(int orderId) {
+        String sql = "DELETE FROM orders WHERE id = ?";
 
-        try {
-            connection = ConnectionFactory.getConnection();
-            statement = connection.prepareStatement(sql);
+        try (Connection connection = ConnectionFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
             statement.setInt(1, orderId);
-            statement.execute();
+            statement.executeUpdate();
 
-        } catch (Exception ex) {
-            throw new RuntimeException("Erro ao deletar a ordem");
-        } finally {
-            ConnectionFactory.closeConnection(connection, statement);
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error while deleting order: " + ex.getMessage(), ex);
         }
     }
 
-    public List<Order> getAll(Long orderId) {
-        
+    public List<Order> getAll(int orderId) {
         String sql = "SELECT * FROM orders WHERE orderId = ?";
         
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet result = null;
-        
-        List<Order> orders = new ArrayList<Order>[];
-        
-        try{
-        connection = ConnectionFactory.getConnection();
-        statement = connection.prepareStatement(sql);
-        statement.setInt(1, orderId);
-        result = statement.executeQuery();
-        
-        while(result.next()){
-            Order order = new Order();
-            order.setId(result.getInt("id"));
-            order.setOrderId(result.getInt("orderId"));
-            order.setType(result.getString("type"));
-        }
-        }catch(Exception ex){
-                
+        List<Order> orders = new ArrayList<>();
+
+        try (Connection connection = ConnectionFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, orderId);
+            try (ResultSet result = statement.executeQuery()) {
+                while (result.next()) {
+                    Order order = new Order();
+                    order.setId(result.getInt("id"));
+                    order.setOrderId(result.getInt("orderId"));
+                    order.setType(result.getString("type"));
+                    // Set other properties here
+                    orders.add(order);
                 }
-        
-        
-        return null;
+            }
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error while retrieving orders: " + ex.getMessage(), ex);
+        }
+
+        return orders;
     }
 }
