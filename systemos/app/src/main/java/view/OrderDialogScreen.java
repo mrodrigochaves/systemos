fd80314ac10090515a64c46e53515ea0ffeb33e5
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-09-01 21:59:45 -0300

    update order dialog

--- systemos/app/src/main/java/view/OrderDialogScreen.java
+++ systemos/app/src/main/java/view/OrderDialogScreen.java
@@ -18,6 +18,10 @@ public class OrderDialogScreen extends javax.swing.JDialog {
         initComponents();
     }
 
+    OrderDialogScreen() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
